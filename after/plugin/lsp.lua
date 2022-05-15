b0d11150c00b6b41b6f0e9e60a0c4ff32041747e
ProgKea <hoschy01@googlemail.com>
2022-05-15 21:11:27 +0200

    added hls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.pyright.setup{}
-require'lspconfig'.sumneko_lua.setup{}
 require'lspconfig'.texlab.setup{}
+require'lspconfig'.hls.setup{}
